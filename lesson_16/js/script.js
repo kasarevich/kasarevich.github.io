aa0e720d3c662a1ac7812586d76d7621b5c3d2b6
kasarevich <33556532+kasarevich@users.noreply.github.com>
2020-01-22 21:02:47 +0300

    Update script.js

--- lesson_16/js/script.js
+++ lesson_16/js/script.js
@@ -1,6 +1,8 @@
  $('.slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
+  focusOnSelect: false,
+  swipeToSlide:true,
   arrows: false,
   fade: true,
   asNavFor: '.slider-nav'
@@ -10,7 +12,8 @@ $('.slider-nav').slick({
   asNavFor: '.slider',
   dots: true,
   centerMode: true,
-  focusOnSelect: true,
+  swipeToSlide:true,
+  focusOnSelect: false,
   autoplay: true,
   autoplaySpeed: 2000,
 });
