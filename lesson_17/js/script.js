d5803eb974a2e964e5490dcd1249ba6e890e9a37
kasarevich <33556532+kasarevich@users.noreply.github.com>
2020-01-24 00:05:45 +0300

    remove inline js

--- lesson_17/js/script.js
+++ lesson_17/js/script.js
@@ -1,19 +1,26 @@
- $('.slider').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  focusOnSelect: false,
-  swipeToSlide:true,
-  arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  asNavFor: '.slider',
-  dots: true,
-  centerMode: true,
-  swipeToSlide:true,
-  focusOnSelect: false,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
+ var modal = document.getElementsByClassName("modal")[0];
+ var remove = document.getElementsByClassName("remove")[0];
+ var image = document.getElementById("image-to-remove");
+ var cancel = document.getElementById("btn-cancel");
+ var confirm = document.getElementById("btn-ok");  
+ var exit = document.getElementsByTagName("span")[0];
+
+ remove.onclick = function(){
+  modal.style.display = "block";
+ }
+ exit.onclick = function(){
+  modal.style.display = "none";
+ }
+ cancel.onclick = function(){
+  modal.style.display = "none";
+ }
+ window.onclick = function(){
+  if(event.target == modal){
+    modal.style.display = "none";
+  }
+ }
+ confirm.onclick = function(){
+  modal.style.display = "none";
+  image.parentNode.removeChild(image);
+  remove.disabled = true;
+ }
